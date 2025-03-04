5e0a12e08d7e2e33b8005ef4e56953a6d8ede925
prakashraj42mk@gmail.com <prakash@quehive.com>
2025-03-04 18:45:08 +0530

    feat : add jwt token and register user

--- app/api.py
+++ app/api.py
@@ -3,19 +3,29 @@ from app import schemas, database
 from app.database import get_db, user_coll, booking_coll
 from bson import ObjectId
 import bcrypt
+from app.auth import decode_access_token
 
 
 router = APIRouter()
+
+
+def get_current_user(token : str):
+
+    payload = decode_access_token(token)
+    if not  payload:
+        raise HTTPException(status_code=401, detail= "invalid token")
+    return payload["user_id"]
+
+
 #Add new user
 @router.post("/user/")
-async def Create_user(user : schemas.UserCreate):
-
-    # existing_user =  user_coll.find_one({"email" : user.email})
-    # if existing_user:
-    #     raise HTTPException (status_code=400, detail= "Email id already exist")
+async def user_registration(user : schemas.UserCreate):
 
 
-    # hashed_password = bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt())
+    existing_user = user_coll.find_one({"email" : user.email})
+    if  existing_user:
+        raise HTTPException(status_code=400 ,detail= "Email already registered")
+
 
     new_user ={
         "name"  : user.name,
@@ -25,7 +35,12 @@ async def Create_user(user : schemas.UserCreate):
     } 
 
     result = user_coll.insert_one(new_user)
-    return {"Done"}
+    return {"message" : "user registerd successfully", "id" : str(result.inserted_id)}
+
+
+
+
+
 
 
 #booking bike
@@ -46,4 +61,3 @@ async def Create_booking(booking : schemas.BookingCreatation, user_id :str):
     return {**new_booking, "id":str (result.inserted_id)}
 
 
-print("som")
