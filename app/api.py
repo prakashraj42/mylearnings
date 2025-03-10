1b935f443f57576d3a194ed92a388ec17a3b3910
Gokul0608 <gokulraja@quehive.com>
2025-03-10 12:47:30 +0530

    fix: authenticated routes for get_users

--- app/api.py
+++ app/api.py
@@ -3,8 +3,10 @@ from  app.schemas import user_register, Login, BikerRegister
 from app.service import user_resgitration, login, get_users, get_bikers,biker_resgitration
 from typing import Optional 
 from app.map import get_address_from_coordinates, get_distance_between_locations, biker_location
-from app.auth import decode_access_token
+from app.auth import decode_access_token, get_current_user
 from fastapi.security import OAuth2PasswordBearer
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.database import get_db
 
 router = APIRouter()
 
@@ -26,12 +28,12 @@ async def user_login(request: Login, responses: Response, user_type : Optional[b
     return await login(login= request, user_type= user_type , responses= responses )
 
 @router.get("/users/", tags=["users"])
-async def get_user():
-    return await get_users()
+async def get_user(token = Depends(get_current_user), db: AsyncIOMotorClient = Depends(get_db)):
+    return await get_users(db=db)
 
 @router.get("/bikers/", tags=["bikers"])
-async def get_biker(token: dict = Depends(decode_access_token)):
-    return await get_bikers(token)
+async def get_biker():
+    return await get_bikers()
 
 @router.get("/Bike_Booking/", tags=["users"])
 async def get_user_location(start, destination):
@@ -43,3 +45,5 @@ async def get_biker_location(start):
     
     return await biker_location(starting_address =  start)
 
+# @router.post("/token")
+# async def 
